2c8b66b47ed04a33e153e8be8c3e6d2f017fa9be
lloise <heloiseeef@gmail.com>
2022-03-25 19:26:58 -0300

    Navbar + Product Card + Catalog + react-router-dom install

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './assets/styles/custom.scss'
+import './App.css';
+
+import Catalog from 'pages/Home/Catalog';
+import ProductCard from 'components/ProductCard';
+
+const App = () => {
+  return (
+ <Catalog />
+
+  );
+}
+
+export default App;
